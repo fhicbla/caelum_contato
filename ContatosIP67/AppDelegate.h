485f5810c97b065c44066f7c2c80f2c684acb937
ios5065 <ios5065@Mac-mini-de-Caelum-4.local>
2015-04-11 14:12:47 -0300

    Initial Commit

---
+++ ContatosIP67/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ContatosIP67
+//
+//  Created by ios5065 on 11/04/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ContatosIP67/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContatosIP67
+//
+//  Created by ios5065 on 11/04/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
